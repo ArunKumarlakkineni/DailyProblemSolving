9c563465a920dd2df2f31f15f5103162d745c7e8
ArunKumar <54982190+ArunKumarlakkineni@users.noreply.github.com>
2025-09-27 12:48:06 +0530

    Time: 99 ms (41.51%) | Memory: 45.6 MB (10.71%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,4 +1,5 @@
 class Solution {
+    Map<String, Integer> memo = new HashMap<>();
     public int solve(int[] nums,int in,int target,int cursum){
         if(in<0 && target == cursum){
             return 1;
@@ -6,9 +7,14 @@ class Solution {
         if(in<0){
             return 0;
         }
+        String key = in + "," + cursum;
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
         int pos = solve(nums,in-1,target,cursum+nums[in]);
         int neg = solve(nums,in-1,target,cursum-nums[in]);
-        return pos+neg;
+        memo.put(key, pos + neg);
+        return memo.get(key);
     }
     public int findTargetSumWays(int[] nums, int target) {
         return solve(nums,nums.length-1,target,0);

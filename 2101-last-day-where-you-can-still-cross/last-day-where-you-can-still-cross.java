2c870f1a4652e72ff4a452023caa076321df155e
ArunKumar <54982190+ArunKumarlakkineni@users.noreply.github.com>
2025-12-31 23:41:14 +0530

    Time: 95 ms (23.86%) | Memory: 78 MB (74.62%) - LeetSync

--- 2101-last-day-where-you-can-still-cross/last-day-where-you-can-still-cross.java
+++ 2101-last-day-where-you-can-still-cross/last-day-where-you-can-still-cross.java
@@ -1,51 +1,51 @@
 class Solution {
-    public boolean solve(int[][] grid, int row, int col, boolean[][] vis) {
-        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length)
-            return false;
-        if (grid[row][col] == 1 || vis[row][col])
-            return false;
+    int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+    private boolean canCross(int row, int col, int[][] cells, int day) {
+        int[][] grid = new int[row][col];
 
-        if (row == 0) return true;
+        for (int i = 0; i < day; i++) {
+            grid[cells[i][0] - 1][cells[i][1] - 1] = 1;
+        }
 
-        vis[row][col] = true;
+        Queue<int[]> q = new ArrayDeque<>();
+        boolean[][] vis = new boolean[row][col];
 
-        return solve(grid, row - 1, col, vis) ||
-               solve(grid, row, col - 1, vis) ||
-               solve(grid, row, col + 1, vis) ||
-               solve(grid, row + 1, col, vis);
-    }
-    public int latestDayToCross(int row, int col, int[][] cells) {
-        int l = 1, r = cells.length;
-        int ans = 0;
-
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-
-            int[][] grid = new int[row][col];
-            for (int i = 0; i < mid; i++) {
-                int[] cell = cells[i];
-                grid[cell[0] - 1][cell[1] - 1] = 1;
-            }
-
-            boolean canCross = false;
-            for (int c = 0; c < col; c++) {
-                if (grid[row - 1][c] == 0) {
-                    boolean[][] vis = new boolean[row][col];
-                    if (solve(grid, row - 1, c, vis)) {
-                        canCross = true;
-                        break;
-                    }
-                }
-            }
-
-            if (canCross) {
-                ans = mid;
-                l = mid + 1;
-            } else {
-                r = mid - 1;
+        for (int c = 0; c < col; c++) {
+            if (grid[0][c] == 0) {
+                q.offer(new int[]{0, c});
+                vis[0][c] = true;
             }
         }
 
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            if (cur[0] == row - 1) return true;
+
+            for (int[] d : dirs) {
+                int nr = cur[0] + d[0];
+                int nc = cur[1] + d[1];
+                if (nr >= 0 && nr < row && nc >= 0 && nc < col &&
+                    !vis[nr][nc] && grid[nr][nc] == 0) {
+                    vis[nr][nc] = true;
+                    q.offer(new int[]{nr, nc});
+                }
+            }
+        }
+        return false;
+    }
+    public int latestDayToCross(int row, int col, int[][] cells) {
+        int left = 1, right = cells.length, ans = 0;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (canCross(row, col, cells, mid)) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
         return ans;
     }
 }

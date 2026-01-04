7250fb0e0f2c4c9eca03b00dda417815871a544b
ArunKumar <54982190+ArunKumarlakkineni@users.noreply.github.com>
2026-01-04 20:25:10 +0530

    Time: 92 ms (32.34%) | Memory: 84.9 MB (18.78%) - LeetSync

--- 2101-last-day-where-you-can-still-cross/last-day-where-you-can-still-cross.java
+++ 2101-last-day-where-you-can-still-cross/last-day-where-you-can-still-cross.java
@@ -2,32 +2,29 @@ class Solution {
     int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
     private boolean canCross(int row, int col, int[][] cells, int day) {
         int[][] grid = new int[row][col];
-
         for (int i = 0; i < day; i++) {
             grid[cells[i][0] - 1][cells[i][1] - 1] = 1;
         }
 
-        Queue<int[]> q = new ArrayDeque<>();
         boolean[][] vis = new boolean[row][col];
-
-        for (int c = 0; c < col; c++) {
-            if (grid[0][c] == 0) {
-                q.offer(new int[]{0, c});
+        Queue<int[]> q = new ArrayDeque<>();
+        for(int c=0;c<col;c++){
+            if(grid[0][c]==0){
+                q.offer(new int[]{0,c});
                 vis[0][c] = true;
             }
         }
+        while(!q.isEmpty()){
+            int[] curr = q.poll();
+            if(curr[0] == row-1) return true;
 
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            if (cur[0] == row - 1) return true;
-
-            for (int[] d : dirs) {
-                int nr = cur[0] + d[0];
-                int nc = cur[1] + d[1];
-                if (nr >= 0 && nr < row && nc >= 0 && nc < col &&
-                    !vis[nr][nc] && grid[nr][nc] == 0) {
+            for(int[] d:dirs){
+                int nr = curr[0]+d[0];
+                int nc = curr[1]+d[1];
+                if(nr>=0 && nr<row && nc>=0 && nc<col 
+                    && !vis[nr][nc] && grid[nr][nc]==0){
                     vis[nr][nc] = true;
-                    q.offer(new int[]{nr, nc});
+                    q.offer(new int[]{nr,nc});
                 }
             }
         }

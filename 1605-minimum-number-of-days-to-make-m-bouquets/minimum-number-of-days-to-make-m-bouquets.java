2afe3b6f3915ab87b02edd97b8ae08752f710e13
ArunKumar <54982190+ArunKumarlakkineni@users.noreply.github.com>
2026-01-11 16:07:36 +0530

    Time: 17 ms (72.84%) | Memory: 82.8 MB (29.75%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,39 +1,46 @@
 class Solution {
-    public boolean check(int[] arr,int m,int k,int mid){
-        int i=0;
-        int kk = k;
-        int n = arr.length;
-        while(i<n){
-            while(i<n && arr[i]<=mid && k>0){
-                k--;
-                i++;
+    public boolean canMakeBouquets(int[] bloomDay, int m, int k, int day) {
+        int bouquets = 0;
+        int flowers = 0;
+
+        for (int i = 0; i < bloomDay.length; i++) {
+            if (bloomDay[i] <= day) {
+                flowers++;
+                if (flowers == k) {
+                    bouquets++;
+                    flowers = 0;
+                }
+            } else {
+                flowers = 0;
             }
-            if(k==0){
-                m--;
-                k=kk;
-            }else{
-                k=kk;
-                i++;
-            }
-            
         }
-        return m<=0;
+
+        return bouquets >= m;
     }
     public int minDays(int[] bloomDay, int m, int k) {
-        if((m*k)>bloomDay.length) return -1;
-        int ans = -1;
-        int n = bloomDay.length-1;
-        int l = Arrays.stream(bloomDay).min().getAsInt();
-        int r = Arrays.stream(bloomDay).max().getAsInt();
-        while(l<=r){
-            int mid = l+(r-l)/2;
-            if(check(bloomDay,m,k,mid)){
-                ans = mid;
-                r = mid-1;
-            }else{
-                l = mid+1;
+        if ((long) m * k > bloomDay.length) return -1;
+
+        int left = Integer.MAX_VALUE;
+        int right = Integer.MIN_VALUE;
+
+        for (int d : bloomDay) {
+            left = Math.min(left, d);
+            right = Math.max(right, d);
+        }
+
+        int answer = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (canMakeBouquets(bloomDay, m, k, mid)) {
+                answer = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
-        return ans;
+
+        return answer;
     }
 }

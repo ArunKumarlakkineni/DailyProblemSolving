a95dec83f66adbe3974f7bd9c740cca6da1a1f2a
ArunKumar <54982190+ArunKumarlakkineni@users.noreply.github.com>
2026-01-11 11:50:45 +0530

    Time: 32 ms (8.58%) | Memory: 46.4 MB (71.66%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
@@ -1,21 +1,35 @@
 class Solution {
-    public int minimumDeleteSum(String s1, String s2) {
-        int total_sum = 0, n1 = s1.length(), n2 = s2.length();
-        for (int i = 0; i < n1; i++) total_sum += s1.charAt(i);
-        for (int i = 0; i < n2; i++) total_sum += s2.charAt(i);
-        
-        int[][] dp = new int[n1 + 1][n2 + 1];
-        for (int i = 1; i <= n1; i++) {
-            for (int j = 1; j <= n2; j++) {
-                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-                    int ascii_value = (int)s1.charAt(i - 1);
-                    dp[i][j] = dp[i - 1][j - 1] + ascii_value;
-                } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-                }
+    int dp[][];
+    public int solve(String s1,String s2,int n1,int n2){
+        if(n1==0 && n2==0) return 0;
+        if(dp[n1][n2]!=-1) return dp[n1][n2];
+        if(n1==0 && n2!=0) {
+            int s2s = 0;
+            for(int k=n2-1;k>=0;k--){
+                s2s+=(int)s2.charAt(k);
             }
+            return s2s;
         }
-
-        return total_sum - 2 * dp[n1][n2];
+        if(n1!=0 && n2==0){
+            int s1s = 0;
+            for(int k=n1-1;k>=0;k--){
+                s1s+=(int)s1.charAt(k);
+            }
+            return s1s;
+        } 
+        if(s1.charAt(n1-1)!=s2.charAt(n2-1)){
+            int del1 = (int)s1.charAt(n1-1) + solve(s1,s2,n1-1,n2);
+            int del2 = (int)s2.charAt(n2-1) + solve(s1,s2,n1,n2-1);
+            return dp[n1][n2] = Math.min(del1,del2);
+        }
+        return dp[n1][n2] = solve(s1,s2,n1-1,n2-1);
+    }
+    public int minimumDeleteSum(String s1, String s2) {
+        int n1 = s1.length(),n2 = s2.length();
+        dp = new int[n1+1][n2+1];
+        for(int i=0;i<n1+1;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return solve(s1,s2,n1,n2);
     }
 }

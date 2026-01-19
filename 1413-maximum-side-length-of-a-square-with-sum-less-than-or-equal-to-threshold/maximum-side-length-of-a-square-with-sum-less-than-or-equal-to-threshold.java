ccd069ebfa6ec7ea289d4a9aa2ee9fc58805aaa4
ArunKumar <54982190+ArunKumarlakkineni@users.noreply.github.com>
2026-01-19 16:40:23 +0530

    Time: 11 ms (27.41%) | Memory: 54.5 MB (89.85%) - LeetSync

--- 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
@@ -1,23 +1,19 @@
 class Solution {
-    public boolean check(int[][] mat,int threshold,int mid){
+    public boolean check(int[][] mat, int threshold, int mid) {
         int m = mat.length;
         int n = mat[0].length;
-        for(int i=0;i<=m-mid;i++){
-            for(int j=0;j<=n-mid;j++){
-                int psum = 0;
-                boolean notValid = false;
-                for(int sqi=i;sqi<(i+mid);sqi++){
-                    for(int sqj=j;sqj<(j+mid);sqj++){
-                        psum+=mat[sqi][sqj];
-                        if(psum>threshold){
-                            notValid = true;
-                            break;
-                        }
-                    }
-                }
-                if(!notValid){
-                    return true;
-                }
+
+        for (int i = 0; i + mid - 1 < m; i++) {
+            for (int j = 0; j + mid - 1 < n; j++) {
+                int r2 = i + mid - 1;
+                int c2 = j + mid - 1;
+
+                int sum = mat[r2][c2];
+                if (i > 0) sum -= mat[i - 1][c2];
+                if (j > 0) sum -= mat[r2][j - 1];
+                if (i > 0 && j > 0) sum += mat[i - 1][j - 1];
+
+                if (sum <= threshold) return true;
             }
         }
         return false;
@@ -25,6 +21,14 @@ class Solution {
     public int maxSideLength(int[][] mat, int threshold) {
         int m = mat.length;
         int n = mat[0].length;
+        // In-place prefix sum
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i > 0) mat[i][j] += mat[i - 1][j];
+                if (j > 0) mat[i][j] += mat[i][j - 1];
+                if (i > 0 && j > 0) mat[i][j] -= mat[i - 1][j - 1];
+            }
+        }
         int l = 1,r = Math.min(m,n);
         int ans = 0;
         while(l<=r){

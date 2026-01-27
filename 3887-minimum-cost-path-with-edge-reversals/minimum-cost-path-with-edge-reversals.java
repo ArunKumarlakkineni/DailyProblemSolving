667630a3f6c91924b2ba4386c5f1adb2b5a76587
ArunKumar <54982190+ArunKumarlakkineni@users.noreply.github.com>
2026-01-27 12:31:18 +0530

    Time: 130 ms (44.53%) | Memory: 193.2 MB (96.76%) - LeetSync

---
+++ 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minCost(int n, int[][] edges) {
+        ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int[] edge:edges){
+            adj.get(edge[0]).add(new int[]{edge[1],edge[2]});
+            adj.get(edge[1]).add(new int[]{edge[0],2*edge[2]});
+        }
+        int[] dist = new int[n];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[0] = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[1]-b[1]);
+        pq.add(new int[]{0,0});
+        while(!pq.isEmpty()){
+            int[] cur = pq.poll();
+            int u = cur[0];
+            int weight = cur[1];
+            for(int[] neigh:adj.get(u)){
+                int v = neigh[0];
+                int newWeight = weight+neigh[1];
+                if(dist[v] > newWeight){
+                    dist[v] = newWeight;
+                    pq.add(new int[]{v,newWeight});
+                }
+            }
+        }
+        return dist[n-1]==Integer.MAX_VALUE?-1:dist[n-1];
+    }
+}

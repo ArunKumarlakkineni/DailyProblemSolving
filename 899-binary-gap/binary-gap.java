c9f087d24bd0062f52ccadbc808c2f1a968cc37e
ArunKumar <54982190+ArunKumarlakkineni@users.noreply.github.com>
2026-02-22 21:33:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (48.75%) - LeetSync

---
+++ 899-binary-gap/binary-gap.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int binaryGap(int n) {
+        int res = 0;
+        int fir = -1,sec = -1;
+        int in = 0;
+        while(n!=0){
+            if((n&1)==1){
+                if(fir==-1){
+                    fir = in;
+                }else if(fir!=-1){
+                    res = Math.max(res,in-fir);
+                    fir = in;
+                }
+            }
+            in++;
+            n=n/2;
+        }
+        return res;
+    }
+}

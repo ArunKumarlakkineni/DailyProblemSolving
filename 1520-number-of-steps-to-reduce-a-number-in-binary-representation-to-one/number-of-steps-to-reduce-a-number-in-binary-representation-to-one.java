5afbedd4289cb045aa621f18be136b9546ae5f9e
ArunKumar <54982190+ArunKumarlakkineni@users.noreply.github.com>
2026-02-26 15:50:38 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (43.36%) - LeetSync

--- 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
@@ -1,39 +1,19 @@
 class Solution {
-    public StringBuilder addOne(StringBuilder s) {
-        int n = s.length();
-        int carry = 1;   // because we are adding 1
-        
-        for (int i = n - 1; i >= 0; i--) {
-            if (carry == 0) break;
-            
-            if (s.charAt(i) == '0') {
-                s.setCharAt(i, '1');
-                carry = 0;   // no more carry
-            } else {
-                s.setCharAt(i, '0');
-                carry = 1;   // carry continues
-            }
-        }
-        
-        if (carry == 1) {
-            s.insert(0, '1');
-        }
-        
-        return s;
-    }
+    
 
     public int numSteps(String s) {
         int n = s.length();
+        int carry = 0;
         int res = 0;
-        StringBuilder sb = new StringBuilder(s);
-        while(!(sb.length() == 1 && sb.charAt(0) == '1')){
-            if(sb.charAt(sb.length()-1)=='1'){
-                sb = addOne(sb);
-            }else{
-                sb.deleteCharAt(sb.length() - 1);
+        for (int i = n - 1; i > 0; i--){
+            if (s.charAt(i) - '0' + carry == 1){
+                res += 2;
+                carry = 1;
             }
-            res++;
+            else{
+                res++;
+            }
         }
-        return res;
+        return res + carry;
     }
 }
